eac70bb4d66c2f38a4b6dfe112664d6481fbe5d2
Documenter.jl <documenter@juliadocs.github.io>
2024-02-07 18:12:33 +0000

    build based on bb59918

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = qSVM","category":"page"},{"location":"#qSVM","page":"Home","title":"qSVM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for qSVM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [qSVM]","category":"page"}]
+}
